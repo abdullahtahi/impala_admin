a834b03336f6d2f2dfc8cdbf8d5bf7700ab6155a
abdullahtahir <tahirabdullah801@gmail.com>
2022-07-04 17:11:17 +0500

    starter deign of impala admin

--- src/view/Login/Login.js
+++ src/view/Login/Login.js
@@ -20,8 +20,15 @@ export default function Login() {
         }
         }
     }
+    const postlogin=()=>{
+     validation();
+        const postCredential={
+email:email,
+password:password
+}
+    }
+    console.log(email,password)
     return (
-
         <Grid>
         <Paper className="loginform" md={3}elevation={3}>
             <Box>
@@ -31,16 +38,23 @@ export default function Login() {
                 <TextField  
                 style={{marginBottom:"10px"}}
                 varient="outlined"
+                onBlur={()=>validation("email",email)}
                 onChange={(e)=>setEmail(e.target.value)}
                 label='Please Enter a email' />
             </Box>
-<Box>{emailerror}</Box>
+<Box style={{color:"red",marginTop:"-10px"}}>{emailerror}</Box>
             <Box className="password">
-            <TextField  varient="outlined" label='Please Enter a password' />
+            <TextField  
+            style={{marginTop:"20px"}}
+            onBlur={()=>validation("password",password)}
+            onChange={(e)=>setpassword(e.target.value)}
+            varient="outlined" label='Please Enter a password' />
             </Box>
 {/* <Box>{emailerror}</Box> */}
             <Box>
-                <Button varient="outlined" style={{marginTop:"10px",border:"1px solid green",color:"green"}}>Login</Button>
+                <Button
+                disabled={email && password ? false:true}
+                onClick={postlogin}varient="outlined" style={{marginTop:"10px",border:"1px solid green",color:"green"}}>Login</Button>
             </Box>
             </Paper>
         </Grid>
